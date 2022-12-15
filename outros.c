bea97a49bd4ba3ec90afe3b35ad7771d188b7801
Marcos Paulo Baptista <60797797+santosbpm@users.noreply.github.com>
2022-12-15 19:59:48 -0300

    Update outros.c

--- outros.c
+++ outros.c
@@ -2,6 +2,10 @@
 
 # include <stdio.h>
 
+/*
+Essa função cria uma pausa no terminal
+e também limpa o buffer com __fpurge()
+*/
 void continuar() {
   printf("Pressione enter para continuar...\n\n");
   __fpurge(stdin);
